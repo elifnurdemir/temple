2c92fa0a351ecf784ae95d63b5540017cf692600
metrosso <necatimertmetin@gmail>
2024-04-06 20:01:38 +0300

    sidebar and cursor fixed

--- src/components/sidebar/templateSidebar.js
+++ src/components/sidebar/templateSidebar.js
@@ -1,27 +1,26 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCamera, faVideo, faFileAlt, faVolumeUp, faLink } from '@fortawesome/free-solid-svg-icons';
-import { faTwitter, faInstagram, faYoutube, faFacebook } from '@fortawesome/free-brands-svg-icons';
 import "../../assets/css/templateSidebar.css";
+import imageIcon from "../../assets/media/landscape-photo-frames-500x500.webp";
 
 const TemplateSidebar = () => {
     const itemTypeList = [
-        { icon: faCamera, title: "Fotograf" },
-        { icon: faVideo, title: "Video" },
-        { icon: faFileAlt, title: "Metin" },
-        { icon: faVolumeUp, title: "Ses" },
-        { icon: faLink, title: "Link" },
-        { icon: faTwitter, title: "Twitter" },
-        { icon: faInstagram, title: "Instagram" },
-        { icon: faYoutube, title: "YouTube" },
-        { icon: faFacebook, title: "Facebook" }
+        { title: "Fotograf", img: imageIcon },
+        { title: "Video" },
+        { title: "Metin" },
+        { title: "Ses" },
+        { title: "Link" },
+        { title: "Twitter" },
+        { title: "Instagram" },
+        { title: "YouTube" },
+        { title: "Facebook" }
     ];
 
     return (
         <div className="template-creation-sidebar">
             {itemTypeList.map((item, index) => (
                 <div key={index} className="template-creation-sidebar-item-type-row">
-                    <FontAwesomeIcon className="template-creation-sidebar-item-type-icon" icon={item.icon} />
+                    {item.img && <img src={item.img} alt="img" />}
                     <p>{item.title}</p>
                 </div>
             ))}
